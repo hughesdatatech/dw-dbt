c798f1e93619379e9cc21e04418622eb36921b00
hughesdatatech <117130151+hughesdatatech@users.noreply.github.com>
2022-11-11 10:29:48 -0500

    test

--- dw/macros/process_log/process_log_handler.sql
+++ dw/macros/process_log/process_log_handler.sql
@@ -1,17 +1,18 @@
-{% macro process_log_handler(component='unknown', job_step='unknown', build_env_metadata1='null', build_env_metadata2='null') %}
+{% macro process_log_handler(component='unknown', job_step='unknown') %}
 
     {%- if job_step == 'on-run-start' or job_step == 'on-run-end' -%}
 
         {%- set create_query -%}
 
             create table if not exists {{ target.schema }}.{{ var('process_log') }} (
+                pipeline_metadata varchar,
+                pipeline_project_metadata varchar,
+                pipeline_schedule_metadata varchar,
                 job_id varchar(500), 
                 job_step_component varchar(500), 
                 job_step varchar(1000), 
                 job_step_info1 varchar, 
                 job_step_info2 varchar,
-                build_env_metadata1 varchar,
-                build_env_metadata2 varchar, 
                 loaded_at timestamp, 
                 inserted_at timestamp
             )
@@ -31,24 +32,26 @@
     {%- set insert_query -%}
 
         insert into {{ target.schema }}.{{ var('process_log') }} (
+            pipeline_metadata,
+            pipeline_project_metadata,
+            pipeline_schedule_metadata,
             job_id, 
             job_step_component, 
             job_step, 
             job_step_info1,
             job_step_info2,
-            build_env_metadata1,
-            build_env_metadata2,
             loaded_at, 
             inserted_at
         ) 
         values(
+            '{{ var("pipeline_metadata") }}',
+            '{{ var("pipeline_project_metadata") }}',
+            '{{ var("pipeline_schedule_metadata") }}',
             '{{ invocation_id }}', 
             {{ "'dbt project: " + project_name + "'" if component == 'job' else remove_double_quotes(this) }},
             '{{ component + ": " + job_step }}',
             {{ "'dbt version: " + dbt_version + "'" if component == 'job' else 'null' }},
             {{ "'clean_all_objects: complete'" if var("run_key") == var("clean_all_objects_key") else 'null' }},
-            '{{ build_env_metadata1 }}',
-            '{{ build_env_metadata2 }}',
             {{ build_loaded_at('null') }}, 
             {{ build_inserted_at('null') }}
         )
