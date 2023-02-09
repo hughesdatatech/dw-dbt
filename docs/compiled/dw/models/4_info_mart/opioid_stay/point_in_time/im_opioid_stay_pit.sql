2c8614661ec83036784a1c6a5c405a41278baa0d
hughesdatatech <117130151+hughesdatatech@users.noreply.github.com>
2023-02-09 15:54:18 -0500

    Update

--- docs/compiled/dw/models/4_info_mart/opioid_stay/point_in_time/im_opioid_stay_pit.sql
+++
@@ -1,5 +0,0 @@
-select
-    *,
-    im_opioid_stay_loaded_at as point_in_time_at
-from 
-    dw_dev.dbt_steve.im_opioid_stay
--- docs/compiled/dw/models/utilities/_utilities__models.yml/process_log_load_succeeded_pro_1baa9a4d0f801ada8cf2c519823dc1ff.sql
+++
@@ -1,9 +0,0 @@
-select
-    *
-from 
-    dw_dev.dbt_steve.process_log_stg_rv_dupe_test
-where true
-    and key_dupes != 0
-    and inserted_at = (
-        select max(inserted_at) from dw_dev.dbt_steve.process_log_stg_rv_dupe_test
-    )
