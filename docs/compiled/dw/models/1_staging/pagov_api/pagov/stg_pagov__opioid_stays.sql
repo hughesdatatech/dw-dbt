34745d2dd84860972384547beb13dfbc76a9ef2b
hughesdatatech <hughesdatatech@gmail.com>
2023-02-09 16:09:51 +0000

    [skip ci]

---
+++ docs/compiled/dw/models/1_staging/pagov_api/pagov/stg_pagov__opioid_stays.sql
@@ -0,0 +1,76 @@
+
+
+with
+
+all_source_rows as (
+
+    select
+        'default' as rv_pagov__opioid_stays_tenant_key,
+    cast(sha2(nvl(trim(cast(county_name as varchar)), '') || '||' || nvl(trim(cast(time_period_date_end as varchar)), ''), 256) as varbinary(64)) rv_pagov__opioid_stays_hk,
+    
+        'dw_dev.pagov.opioid_stays' as rv_pagov__opioid_stays_rec_source,
+    
+    '86a68623-112c-440d-a877-88efe1918ec9' as rv_pagov__opioid_stays_job_id,
+    'circleci' as rv_pagov__opioid_stays_job_user_id,
+    'default' as rv_pagov__opioid_stays_jira_task_key,
+    to_char(timestamp '2023-02-09 16:09:44.334904+00:00', 'YYYY-MM-DD HH24:MI:SS')::timestamp  as rv_pagov__opioid_stays_extracted_at,
+    to_char(timestamp '2023-02-09 16:09:44.334904+00:00', 'YYYY-MM-DD HH24:MI:SS')::timestamp rv_pagov__opioid_stays_loaded_at,
+    cast(sha2(nvl(trim(cast(COUNT_OF_MATERNAL_STAYS_WITH as varchar)), '') || '||' || nvl(trim(cast(COUNTY_CODE_NUMBER as varchar)), '') || '||' || nvl(trim(cast(COUNTY_FIPS_CODE as varchar)), '') || '||' || nvl(trim(cast(FIPS_COUNTY_CODE as varchar)), '') || '||' || nvl(trim(cast(GEOCODED_COLUMN_COORDINATES as varchar)), '') || '||' || nvl(trim(cast(GEOCODED_COLUMN_TYPE as varchar)), '') || '||' || nvl(trim(cast(LATITUDE_LONGITUDE as varchar)), '') || '||' || nvl(trim(cast(RATE_OF_MATERNAL_STAYS_WITH as varchar)), '') || '||' || nvl(trim(cast(STATE_FIPS_CODE as varchar)), '') || '||' || nvl(trim(cast(TIME_PERIOD as varchar)), '') || '||' || nvl(trim(cast(TIME_PERIOD_DATE_START as varchar)), '') || '||' || nvl(trim(cast(TYPE_OF_COUNT as varchar)), '') || '||' || nvl(trim(cast(TYPE_OF_RATE as varchar)), ''), 256) as varbinary(64)) rv_pagov__opioid_stays_hd
+    ,
+        COUNT_OF_MATERNAL_STAYS_WITH, 
+		COUNTY_CODE_NUMBER, 
+		COUNTY_FIPS_CODE, 
+		COUNTY_NAME, 
+		FIPS_COUNTY_CODE, 
+		GEOCODED_COLUMN_COORDINATES, 
+		GEOCODED_COLUMN_TYPE, 
+		LATITUDE_LONGITUDE, 
+		RATE_OF_MATERNAL_STAYS_WITH, 
+		STATE_FIPS_CODE, 
+		TIME_PERIOD, 
+		TIME_PERIOD_DATE_END, 
+		TIME_PERIOD_DATE_START, 
+		TYPE_OF_COUNT, 
+		TYPE_OF_RATE,
+        
+        
+        row_number() over (
+            partition by county_name,time_period_date_end
+            order by 1 desc
+        ) as row_num
+    from 
+        dw_dev.pagov.opioid_stays
+        
+)
+
+select
+    rv_pagov__opioid_stays_tenant_key,
+    rv_pagov__opioid_stays_hk,
+    rv_pagov__opioid_stays_rec_source,
+    rv_pagov__opioid_stays_job_id,
+    rv_pagov__opioid_stays_job_user_id,
+    rv_pagov__opioid_stays_jira_task_key,
+    rv_pagov__opioid_stays_extracted_at,
+    rv_pagov__opioid_stays_loaded_at
+    , rv_pagov__opioid_stays_hd,
+    COUNT_OF_MATERNAL_STAYS_WITH, 
+	COUNTY_CODE_NUMBER, 
+	COUNTY_FIPS_CODE, 
+	COUNTY_NAME, 
+	FIPS_COUNTY_CODE, 
+	GEOCODED_COLUMN_COORDINATES, 
+	GEOCODED_COLUMN_TYPE, 
+	LATITUDE_LONGITUDE, 
+	RATE_OF_MATERNAL_STAYS_WITH, 
+	STATE_FIPS_CODE, 
+	TIME_PERIOD, 
+	TIME_PERIOD_DATE_END, 
+	TIME_PERIOD_DATE_START, 
+	TYPE_OF_COUNT, 
+	TYPE_OF_RATE
+    
+    
+from 
+    all_source_rows
+where true
+    and row_num = 1 
---
+++ docs/compiled/dw/models/4_info_mart/opioid_stay/current/im_opioid_stay.sql
@@ -0,0 +1,23 @@
+
+
+select
+    'default' as im_opioid_stay_tenant_key,
+    cast(sha2(nvl(trim(cast(br_opioid_stay_hk as varchar)), ''), 256) as varbinary(64)) im_opioid_stay_hk,
+    
+        'dw_dev.dbt_steve.br_opioid_stay' as im_opioid_stay_rec_source,
+    
+    '86a68623-112c-440d-a877-88efe1918ec9' as im_opioid_stay_job_id,
+    'circleci' as im_opioid_stay_job_user_id,
+    jira_task_key as im_opioid_stay_jira_task_key,
+    to_char(timestamp '2023-02-09 16:09:44.334904+00:00', 'YYYY-MM-DD HH24:MI:SS')::timestamp  as im_opioid_stay_extracted_at,
+    to_char(timestamp '2023-02-09 16:09:44.334904+00:00', 'YYYY-MM-DD HH24:MI:SS')::timestamp im_opioid_stay_loaded_at,
+    'im_opioid_stay_tenant_key = ' || nvl(im_opioid_stay_tenant_key, '') || '; ' ||
+    'im_opioid_stay_hk = ' || nvl(im_opioid_stay_hk::varchar, '') || '; ' ||
+    'im_opioid_stay_job_id = ' || nvl(im_opioid_stay_job_id, '') || '; ' ||
+    'im_opioid_stay_job_user_id = ' || nvl(im_opioid_stay_job_user_id, '') || '; ' ||
+    'im_opioid_stay_jira_task_key = ' || nvl(im_opioid_stay_jira_task_key, '') || '; ' ||
+    'im_opioid_stay_extracted_at = ' || nvl(im_opioid_stay_extracted_at::varchar, '') || '; ' ||
+    'im_opioid_stay_loaded_at = ' || nvl(im_opioid_stay_loaded_at::varchar, '') as im_opioid_stay_metadata,
+    *
+from 
+    dw_dev.dbt_steve.br_opioid_stay
